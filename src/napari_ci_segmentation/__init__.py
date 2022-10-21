97a22cf18ac9c148bdff3e060175f3a4dda4989c
Sean Martin <seankieran.m@hotmail.com>
2022-10-21 12:02:30 +0100

    Update project layout and segmentation

--- src/napari_ci_segmentation/__init__.py
+++ src/napari_ci_segmentation/__init__.py
@@ -1,14 +1,7 @@
 __version__ = "0.0.1"
 
-from ._reader import napari_get_reader
-from ._sample_data import make_sample_data
 from ._widget import segmentation_widget
-from ._writer import write_multiple, write_single_image
 
 __all__ = (
-    "napari_get_reader",
-    "write_single_image",
-    "write_multiple",
     "segmentation_widget",
-    "make_sample_data",
 )
--- src/napari_ci_segmentation/_widget.py
+++ src/napari_ci_segmentation/_widget.py
@@ -10,31 +10,25 @@ from typing import TYPE_CHECKING
 
 from magicgui import magic_factory
 import numpy as np
-from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget
 from napari.qt.threading import thread_worker
+from napari.utils.notifications import show_info
 
-from skimage import (
-    color,
-    feature,
-    filters,
-    measure,
-    morphology,
-    segmentation,
-    util,
-)
+from skimage import color, filters, morphology
 
 if TYPE_CHECKING:
     import napari
 
 
 @thread_worker
-def segment(image: "napari.types.Image"):
+def segment(
+    image: "napari.types.Image", blur_sigma: float = 2.0, disk_size: int = 4
+):
     image_data = np.asarray(image.data)
-    image_data = blur_and_gray_image(image_data)
+    image_data = blur_and_gray_image(image_data, blur_sigma)
     yield (f"{image.name}_blurred", image_data, True)
     labels = initial_segment(image_data)
     yield (f"{image.name}_rough_segmented", labels, False)
-    labels = smooth_labels(labels)
+    labels = smooth_labels(labels, disk_size)
     yield (f"{image.name}_segmented", labels, False)
 
 
@@ -56,30 +50,52 @@ def initial_segment(
 
 
 def smooth_labels(
-    labels: "napari.types.LabelsData",
-    smooth_threshold: int = 20,
-    disk_size: int = 4,
-):
+    labels: "napari.types.LabelsData", disk_size: int = 4
+) -> "napari.types.LabelsData":
     labels = np.asarray(labels)
-    smoother_labels = filters.rank.mean(labels, morphology.disk(disk_size))
-
-    return smoother_labels
+    return filters.rank.mean(labels, morphology.disk(disk_size))
 
 
-@magic_factory
-def segmentation_widget(viewer: "napari.Viewer", image: "napari.layers.Image"):
-    def display_layer(args):
+@magic_factory(persist=True)
+def segmentation_widget(viewer: "napari.Viewer", image: "napari.layers.Image", blur_sigma: float = 2.0, disk_size: int = 4):
+    """
+    Segment cells from calcium image max projection.
+
+    1. Grayscale and blur the image.
+    2. Perform a multiotsu to separate the image into three classes:
+        background, non-cell tissue, and cells.
+    3. Smooth the multiotsu segmentation via disks.
+
+    Parameters
+    ----------
+    viewer : napari.Viewer
+        A viewer instance from napari
+    image : napari.layers.Image
+        The image to segment from. Can be one channel or more.
+        If more than one channel, is converted to grayscale.
+    blur_sigma : float, optional
+        The standard deviation of the gaussian blur to be applied.
+        A higher sigma indicates more blur.
+        By default 2.0.
+    disk_size : int, optional
+        The size of the disk in pixels for smoothing.
+        A higher disk_size indicates more smoothing.
+        By default 4.
+
+    """
+    def display_layer(args) -> "napari.types.LayerDataTuple":
         name, data, as_image = args
-        print(f"adding {data} to viewer")
         if name in viewer.layers:
+            show_info(f"Updating {name} with new data")
             viewer.layers[name].data = data
         else:
-            added = (
+            show_info(f"Adding {name} to viewer")
+            (
                 viewer.add_image(data, name=name)
                 if as_image
                 else viewer.add_labels(data, name=name)
             )
 
-    worker = segment(image)
+    worker = segment(image, blur_sigma, disk_size)
     worker.yielded.connect(display_layer)
     worker.start()
